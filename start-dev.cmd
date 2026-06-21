174c4b8401e303bd286966434e2cc26acf854e37
Gen <130524857+gg117e@users.noreply.github.com>
2026-06-22 00:55:05 +0900

    Add Windows start script

---
+++ start-dev.cmd
@@ -0,0 +1,8 @@
+@echo off
+cd /d "%~dp0"
+echo Starting Pattern Speak at http://127.0.0.1:3000
+echo.
+echo Keep this window open while using the app.
+echo Press Ctrl+C to stop.
+echo.
+npm.cmd run dev:local
